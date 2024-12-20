c28c65563691e4ef1a70e94c9a1f967a0a8b9580
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-20 12:58:47 +0200

    For and For Each

--- ASS03/Program.cs
+++ ASS03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ASS03
+﻿using System.Globalization;
+
+namespace ASS03
 {
     internal class Program
     {
@@ -21,8 +23,24 @@
             ////time 3*n+2 less performance and best to cleancode and maintainability
             //for(int i=1;i<=10;i++)
             //    Console.WriteLine($"{i} T");
-       
 
+
+            #endregion
+            #region For _For each
+            //int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //continue
+            //for (int i = 0; i < Numbers.Length; i++) {
+            //    Numbers[i] += 10;
+            // if (Numbers[i] == 5) break;
+            //    Console.WriteLine(Numbers[i]);
+            //}
+            ////take copy from array
+            //class implement interface iEnumerable
+            //Array
+            //foreach (int Num in Numbers) {
+            //    //Num += 10;
+            //    Console.WriteLine(Num + 10);
+            //}     
             #endregion
         }
     }
