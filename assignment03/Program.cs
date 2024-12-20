79c59b67f0d7b8783532377e0cab793b1a6e530c
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-21 01:12:49 +0200

    Q4

--- assignment03/Program.cs
+++ assignment03/Program.cs
@@ -32,18 +32,35 @@
             //}
             #endregion
             #region Q3:Write a program that allows to user to insert number then print all even numbers between 1 to this number
-           // int num;
-           //bool f = false;
+            // int num;
+            //bool f = false;
+            // do
+            // {
+            //     Console.Write("Enter a Number:");
+            //     f = int.TryParse(Console.ReadLine(), out num);
+            // } while (!f);
+            // for(int i = 2; i <= num; i++)
+            // {
+            //     if(i%2==0)
+            //         Console.WriteLine(i);
+            // }
+            #endregion
+            #region  Q4: Write a program that takes two integers then prints the power.
+           // int Base , power,res=1;
+           // bool f ,f1= false;
+           //  do
+           //  {
+           //      Console.Write("Enter a base:");
+           //      f = int.TryParse(Console.ReadLine(), out Base);
+           //  } while (!f);
            // do
            // {
-           //     Console.Write("Enter a Number:");
-           //     f = int.TryParse(Console.ReadLine(), out num);
-           // } while (!f);
-           // for(int i = 2; i <= num; i++)
-           // {
-           //     if(i%2==0)
-           //         Console.WriteLine(i);
-           // }
+           //     Console.Write("Enter a power:");
+           //     f1 = int.TryParse(Console.ReadLine(), out power);
+           // } while (!f1);
+           // for (int i = 0; i < power; i++)
+           //     res *= Base;
+           //Console.WriteLine(res);
             #endregion
         }
     }
