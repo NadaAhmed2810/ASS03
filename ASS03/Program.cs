84008a8c6c418539a3cd98e282ee90d81f6a5c41
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-20 12:43:13 +0200

    Loop statement

--- ASS03/Program.cs
+++ ASS03/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Loop statements
+            ////freq and heard to maintainability
+            //Console.WriteLine("1T");
+            //Console.WriteLine("2T");
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+
+
+            ////time 3*n+2 less performance and best to cleancode and maintainability
+            //for(int i=1;i<=10;i++)
+            //    Console.WriteLine($"{i} T");
+       
+
+            #endregion
         }
     }
 }
